1aa7e124030637f416de204e4904707b6e2afb24
Boustique <74506532+soufiane98b@users.noreply.github.com>
2021-10-05 23:18:28 +0200

    Delete javaavance/td1 directory

--- javaavance/td1/Main.java
+++
@@ -1,7 +0,0 @@
-package fr.dauphine.javaavance.td1;
-
-public class Main { 
-	int foo;
-	public static void main(String args[]) {
-	}
-}
--- javaavance/td1/TestPoint.java
+++
@@ -1,9 +0,0 @@
-package fr.dauphine.javaavance.td1;
-
-public class TestPoint {
-	public static void main(String args[]) {
-		//Point p=new Point();
-		//System.out.println(p.x+" "+p.y);
-	}
-
-}
